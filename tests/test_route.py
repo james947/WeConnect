db75a107fcde1ecdf9eda41a7720890ee99ff761
james947 <jmuriuki947@gmail.com>
2018-03-04 16:35:43 -0800

    Add Teststo busineess route

--- tests/test_route.py
+++ tests/test_route.py
@@ -7,7 +7,8 @@ class TestIntegrations(TestCase):
 
     def test_users(self):
         response = self.app.get('http://127.0.0.1:5000/api/v1/users')
-        self.assertEqual(response.status_code,404)
+        self.assertEqual(response.status_code,200)
 
     def test_business(self):
-        resposn
+        response=self.app.post('http://127.0.0.1:5000/api/auth/v1/register')
+        self.assertEqual(response.status_code,201)
