b649c7f67852384937650bfb857149e95ad3f59b
james947 <jmuriuki947@gmail.com>
2018-03-04 17:06:59 -0800

    [REFACTOR]code

--- tests/test_route.py
+++ tests/test_route.py
@@ -13,13 +13,14 @@ class TestIntegrations(TestCase):
     def test_users_registration(self):    
         response = self.app.post('http://127.0.0.1:5000/api/auth/v1/register')
         response_msg = json.loads(response.data)
-        self.assertIn("User successfully registered", response_msg["Message"])  
-    def test_business_registartsion(self):
-        response=self.app.post('http://127.0.0.1:5000/api/auth/v1/business')
-        self.assertEqual(response.status_code,201)
-        
+        self.assertIn("User successfully registered", response_msg["Message"]) 
+
     def test_login(self):
         response= self.app.post('http://127.0.0.1:5000/api/auth/v1/login')
+        self.assertEqual(response.status_code,201)
+ 
+    def test_business_registartsion(self):
+        response=self.app.post('http://127.0.0.1:5000/api/auth/v1/business')
         self.assertEqual(response.status_code,201)
 
     def test_return_all_business(self):
