af02859489adc94d026da2540344cef3e02209b7
james947 <jmuriuki947@gmail.com>
2018-03-05 16:02:14 -0800

    [Finishes #155701687]tests for routes and business.py

--- tests/test_business.py
+++ tests/test_business.py
@@ -6,6 +6,7 @@ class TestIntegrations(TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.business = {
+            "id":1,
             "businessname":"Techbase",
             "description":"we sell laptops",
             "category":"electronics",
@@ -15,23 +16,50 @@ class TestIntegrations(TestCase):
         del self.business
         pass
 
-
-
     def test_business_registration(self):
-    """test business successfully is registered """
-    response=self.app.post('http://127.0.0.1:5000/api/auth/v1/business', data=self.business)
-    self.assertEqual(response.status_code,200)
-    response_msg = json.loads(response.data.decode("UTF-8"))
-    self.assertIn("Business successfully registered", response_msg["Message"]) 
+        #test business successfully is registered 
+        response=self.app.post('http://127.0.0.1:5000/api/auth/v1/business', data=self.business)
+        self.assertEqual(response.status_code,200)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Business successfully registered", response_msg["Message"]) 
 
     def test_returns_all_businesses(self):
-    """test all businesses are returned"""
-    response=self.app.post('http://127.0.0.1:5000/api/auth/v1/business', data=self.business)
-    self.assertEqual(response.status_code,201)
-    response = self.app.get('http://127.0.0.1:5000/api/auth/v1/business')
-    self.assertIn('we sell laptops', str(response.data))
+        #test all businesses are returned
+        response=self.app.post('http://127.0.0.1:5000/api/auth/v1/business', data=self.business)
+        self.assertEqual(response.status_code,201)
+        response = self.app.get('http://127.0.0.1:5000/api/auth/v1/business')
+        self.assertIn('we sell laptops', str(response.data))
 
     def test_api_can_get_business_by_id(self):
-    response = self.app.post('http://127.0.0.1:5000/api/auth/v1/business')
-    self.assertEqual(response.status_code,201)
+        #get business by id
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/business',data=self.business)
+        self.assertEqual(response.status_code,201)
+        result_in_json = json.loads(response.data.decode('utf-8').replace("'", "\""))
+        result = self.app.get('http://127.0.0.1:5000/api/auth/v1/business/{}'.format(result_in_json['id']))
+        self.assertEqual(result.status_code,200)
+        self.assertIn('we sell laptops', str(result.data))
+
+    def test_get_bad _request(self):
+        #test if bad request is passed
+        response=self.app.get('http://127.0.0.1:5000/api/auth/v1/business/biz')
+        self.assertEqual(result.status.code,400)
+
+    def get_by_ivalid_id(self):
+        #tests if the id is not valid
+        response=self.app.get('http://127.0.0.1:5000/api/auth/v1/business/4')
+        self.assertEqual(response.status_code, 404)
+        response_msg = json.loads(response.data)
+        self.assertIn("Business not found", response_msg["Message"])
+
+    def test_add_empty_business_name(self):
+        reponse=self.app.post('http://127.0.0.1:5000/api/auth/v1/business/biz', data=dict(businessname=""))
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Business name cannot be empty", response_msg['Message'])
+
+
+#make tests run
+if __name__ == "__main__":
+   unittest.main()
+
+
 
--- tests/test_route.py
+++ tests/test_route.py
@@ -5,8 +5,14 @@ import json
 class TestIntegrations(TestCase):
     def setUp(self):
         self.app = app.test_client()
+        self.person={
+            'username':'james muriuki',
+            'email':'james20@yahoo.com',
+            'password':'123456'
+                    }
 
     def teardown(self):
+        del self.person
         pass
 
     def test_users_in_data(self):
@@ -16,23 +22,72 @@ class TestIntegrations(TestCase):
 
     def test_users_registration(self):    
         """tests user registration in the system"""
-        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/register', data=json.dumps(dict(username="test122",password=122)),content_type="application/json")
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/register',
+        data=self.person,content_type="application/json")
+        self.assertEqual(response.status_code,201)
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn("User successfully registered", response_msg["Message"]) 
+    
+    def test_if_null_registration_username(self):
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/register',
+        data=dict(name="",email="james20@yahoo.com",password=123456),content_type="application/json")
+        self.assertEqual(response.status_code,400)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Username cannot be null",response_msg["Message"])
+        
+    def test_if_null_registration_password(self):
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/register',
+        data=dict(name="james",email="james20@yahoo.com",password=""),content_type="application/json")
+        self.assertEqual(response.status_code,400)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Password cannot be null",response_msg["Message"])  
 
     def test_login(self):
         """returns correct login"""
         response= self.app.post('http://127.0.0.1:5000/api/auth/v1/login',
         data=json.dumps(dict(username="test122",password=122)),content_type="application/json")
-        self.assertEqual(response.status_code,201)
+        self.assertEqual(response.status_code,200)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Successful Logged in",response_msg["Message"])
         
+    def test_login_without_user_name(self):
+        """tets if API returns an error upon login without username"""
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/register',
+        data=dict(username="test122",password=122),content_type="application/json")
+        response= self.app.post('http://127.0.0.1:5000/api/auth/v1/login',
+        data=dict(username="",password=122),content_type="application/json")
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response.status_code,400)
+        self.assertIn("Username cannot be null",response_msg["Message"])
 
-    def test_return_all_business(self):
-        response =self.app.get('http://127.0.0.1:5000/api/v1/business')
-        self.assertEqual(response.status_code,200)
+    def test_login_with_an_empty_password(self):
+        """test if API returns an error upon login with a null password"""
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/register',
+        data=dict(username="test122",password=122),content_type="application/json")
+        response= self.app.post('http://127.0.0.1:5000/api/auth/v1/login',
+        data=dict(username="test122",password=""),content_type="application/json")
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response.status_code,400)
+        self.assertIn("Password cannot be null",response_msg["Message"])
 
-    def test_return_one_business(self):
-        response =self.app.get('http://127.0.0.1:5000/api/v1/business/1')
-        self.assertEqual(response.status_code,200)
-    
+    def test_login_with_a_wrong_password(self):
+        """tests API if login Works With A wrong password"""
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/register',
+        data=dict(username="test122",password=122),content_type="application/json")
+        response= self.app.post('http://127.0.0.1:5000/api/auth/v1/login',
+        data=dict(username="test122",password=555),content_type="application/json")
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response.status_code,400)
+        self.assertIn("Password not correct",response_msg["Message"])
+
+    def test_login_with_a_wrong_username(self):
+        """tets if API accepts login with a wrong username"""
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/register',
+        data=dict(username="test122",password=122),content_type="application/json")
+        response= self.app.post('http://127.0.0.1:5000/api/auth/v1/login',
+        data=dict(username="james",password=555),content_type="application/json")
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual(response.status_code,400)
+        self.assertIn("Password not correct",response_msg["Message"])
+
 
