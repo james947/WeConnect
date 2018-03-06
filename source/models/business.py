7958a7d4e6dd1d35b3affde5b9f034b7cb5ff4a1
james947 <jmuriuki947@gmail.com>
2018-03-06 04:42:45 -0800

    [ADD]business.py in models folder

---
+++ source/models/business.py
@@ -0,0 +1,11 @@
+class Business():
+    #initialize count to add when business is registered
+    count = 1
+    def __init__(self,name,description,location,category):
+        self.name = name
+        self.description = description
+        self.location = location
+        self.category = category
+        self.id = Business.count
+        Business.count +=1
+
