1e25db3d557b86564d758fe9e464dbae6bbe294d
james947 <jmuriuki947@gmail.com>
2018-03-05 23:17:22 -0800

    [Finishes  #155701687]reviews.py

---
+++ tests/test_reviews.py
@@ -0,0 +1,62 @@
+from source.api import app
+from unittest import TestCase
+import json
+
+class TestIntegrations(TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.reviews = {
+            'title':"App on point",
+            'descritpion':'Your app is awesome',
+        }
+    
+    def teardown(self):
+        del self.reviews
+
+    def test_add_new_review(self):
+        """
+        Api creates a new review
+
+        """
+        response=self.app.post('http://127.0.0.1:5000/api/auth/v1/business/1/reviews', data=self.reviews)
+        self.assertEqual(response.status_code,201)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("review added", response_msg["Message"]) 
+
+    def test_add_null_review_title(self):
+        """"
+        tests APi result if preview title is null
+        """
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/business/1/reviews',data=dict(title="",descritpion="Your app is great"))
+        self.assertEqual(response.status_code,403)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Title is required", response_msg["Message"]) 
+
+    def test_add_null_review_description(self):
+        """"
+        tests APi result if preview title is null
+        """
+        response = self.app.post('http://127.0.0.1:5000/api/auth/v1/business/1/reviews',data=dict(title="",descritpion="Your app is great"))
+        self.assertEqual(response.status_code,404)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertIn("Title is required", response_msg["Message"]) 
+
+
+    def test_get_review(self):
+        """
+        Test if APi gets review
+
+        """
+        response=self.app.post('http://127.0.0.1:5000/api/auth/v1/business/1/reviews', data=self.reviews)
+        self.assertEqual(response.status_code,201) 
+        response=self.app.get('http://127.0.0.1:5000/api/auth/v1/business/1/reviews/1')
+        response = self.assertEqual(response.status_code,200)
+
+    def test_get_invalid_review(self):
+        """
+        Tests Api when request is invalid
+        """
+        response=self.app.post('http://127.0.0.1:5000/api/auth/v1/business/1/reviews', data=self.reviews)
+        self.assertEqual(response.status_code,201)
+        response=self.app.get('http://127.0.0.1:5000/api/auth/v1/business/1/reviews/5')
+        response = self.assertEqual(response.status_code,400)
