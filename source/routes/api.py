b0b3536792380043596b2e1d01d594035bda113a
james947 <jmuriuki947@gmail.com>
2018-03-07 04:19:25 -0800

    [FINISHES #155700332]Add user registration functionality

--- source/routes/api.py
+++ source/routes/api.py
@@ -15,7 +15,7 @@ Enables use of token
 
 """
 
-USERS=[]
+user_instance = User()
 # def add_user(username,email,password,public_id):
 #     new_user=dict(username=username,email=email,password=password,public_id=public_id)
 #     USERS.append(new_user)
@@ -25,43 +25,46 @@ def create_user():
     """
     creates a new user in the list of users
     """
-    username=request.get_json('username')
-    email = request.get_json('email')
-    password=request.get_json('password')
-    """
-    retrieves data from request and stores in data variable
-    """
-    if username['username'] == "":
-        return jsonify({'Message':'Username cannot be null'})
-    elif password['password']=="":
-        return jsonify({'Message':'Password cannot be null'})
-    elif email['email']=="":
-        return jsonify({'Message':'email cannot be null'})
+    user=request.get_json()
+    email = user['email']
+    username = user['username']
+    password= user['password']
 
+    if not email or not username or not password:
+        return jsonify({'message':'Invalid input, please try again!'})
 
-    new_user = User(public_id=str(uuid.uuid4()),username=username['username'],email=email['email'],password=sha256_crypt.encrypt(str(password['password'])))
-    USERS.append(new_user)
+    password=sha256_crypt.encrypt(str(password))
+    print (password)
+    user_instance.create_user(id, username, email, password)
+    print(user_instance.users)
     return jsonify({'Message':'User successfully registered'}),201
 
 @app.route('/api/auth/login', methods = ['POST'])
 def login():
-    auth = request.authorization
-    if not auth or not auth.username or not auth.password:
-        return jsonify({"Message": "Authentication needed"}), 401
-    
-    for user in USERS:
-        if auth.username == user['username'] and auth.password == user['password']:
+    user=request.get_json()
+    username = user['username']
+    password =user['password']
+    for user in user_instance.users:
+       if user.username['username'] == "" or  user.password['password'] == "":
+            return jsonify({'message': 'Username / password ivalid, please try again!'})
+    return jsonify({'message':'logged in successfully'}),201
             
-            token = jwt.encode({'username': auth.username,  'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=25)}, app.config['SECRET_KEY'])
-            
-            return jsonify({"token": token.decode("UTF-8")}), 200
-        return jsonify({'Message':'User not found'})
 
 
 @app.route('/api/auth/users', methods=['GET'])
 def get_all_users():
-    return jsonify({"users": USERS}), 200
+    return jsonify({"users": user_instance.users}), 200
 
 @app.route('/api/v1/business', methods=['POST'])
 def register_business():
-    new_business= Business()
+    new_business = request.get_json()
+    business = new_business['businessname']
+    description=new_business['description']
+    category =new_business['category']
+    location =new_business['location']
+
+    if not business or not description or not category or not location:
+         return jsonify({'message':'Invalid input, please try again!'})
+
+
+    
