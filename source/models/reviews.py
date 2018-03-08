1405eec7cca35f11e7a25c74308b320799a459c0
james947 <jmuriuki947@gmail.com>
2018-03-08 07:31:10 -0800

    [FIXES #155822781]reviews

--- source/models/reviews.py
+++ source/models/reviews.py
@@ -1,17 +1,6 @@
 class Reviews():
-    """initialize an empty dict to store reviews"""
-    def __init__(self):
-        self.reviews = []
-
-    def create_user(self, id, username,title,reviews):
-        """Create user then append to list reviews"""
-        new_reviews = {
-            'id':id,
-            'username': username,
-            'title':title,
-            'review':reviews
-
-        }
-
-        self.reviews.append(new_reviews)
-       
+    count = 1
+    def __init__(self,review):
+        self.review = review
+        self.id = Reviews.count
+        Reviews.count+=1
