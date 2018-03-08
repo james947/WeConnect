ccec441614a53c8705324c6d8e97918d749fd909
james947 <jmuriuki947@gmail.com>
2018-03-08 07:26:09 -0800

    [FIXES #155822781]business.py

--- source/models/business.py
+++ source/models/business.py
@@ -1,20 +1,9 @@
 class Business():
-    #initialize count to add when business is registered
-    count =0
-    def __init__(self):
-        self.business=[]
-
-    def create_business(self,id,businessname,description,location,category):
-
-        new_business={
-            'id':Business.count + 1,
-            'businessname':businessname,
-            'description':description,
-            'location':location,
-            'category':category,
-                }
-
-        self.business.append(new_business)
+    count = 1
+    def __init__(self,name,description,location,category):
+        self.name = name
+        self.description = description
+        self.location = location
+        self.category = category
+        self.id = Business.count
         Business.count +=1
-
-        return self.business
