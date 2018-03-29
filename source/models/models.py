f4708c1eff33cb6f3a760a1176829eec7265a5ea
james947 <jmuriuki947@gmail.com>
2018-03-29 12:37:32 +0300

    [REFACTOR] model classes

---
+++ source/models/models.py
@@ -0,0 +1,38 @@
+from source.routes.api import db
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    """create table users"""
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+
+class Business(db.model):
+    __tablename__ = 'business'
+    id = db.Column(db.Integer, primary_key=True)
+    businessname = db.Column(db.String(50), unique=True)
+    description = db.Column(db.String(50), unique=True, nullable=False)
+    category = db.Column(db.String(50), unique=True, nullable=False)
+    location = db.Column(db.String(50), unique=True, nullable=False)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
+
+
+
+class Reviews(db.model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    title= db.Column(db.String(50), unique=True, nullable=False)
+    review= db.Column(db.String(50), unique=True, nullable=False)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(
+        db.DateTime, default=db.func.current_timestamp(),
+        onupdate=db.func.current_timestamp())
