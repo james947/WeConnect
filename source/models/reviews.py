380c7888e117e2347bd27e5d16e45e08da6e011a
james947 <jmuriuki947@gmail.com>
2018-03-29 11:09:40 +0300

    [REFACTOR]delete previous models to implement db

--- source/models/reviews.py
+++
@@ -1,9 +0,0 @@
-class Reviews():
-    """increments review by one"""
-    count = 0
-    def __init__(self,title,description,businessid):
-        self.title = title
-        self.description= description
-        self.businessid= businessid
-        self.id= Reviews.count
-        Reviews.count+=1
