212567c82f2faa76b348cc524ef1ee24c0303ba2
james947 <jmuriuki947@gmail.com>
2018-03-29 12:34:44 +0300

    Add entry point

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from source.routes.api import app#import app from source folder
+from source.routes.api import create_app#import app from source folder
+import os
 
+"get develoments settings from the env variable"
+config_name = os.getenv('APP_SETTINGS')
+app = create_app(config_name)
 
 #starts the app
 if __name__ == "__main__":
-     app.run(debug=True)
+     app.run()
