310b2abd735977d96a4d5c4966ad66e778791090
james947 <jmuriuki947@gmail.com>
2018-03-30 14:03:55 +0300

    [FIX]remove unique key constraints

--- source/models/models.py
+++ source/models/models.py
@@ -5,7 +5,8 @@ class Users(db.Model):
     """create table users"""
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, 
@@ -20,9 +21,9 @@ class Business(db.Model):
     __tablename__ = 'business'
     id = db.Column(db.Integer, primary_key=True)
     businessname = db.Column(db.String(50), unique=True)
-    description = db.Column(db.String(50), unique=True, nullable=False)
-    category = db.Column(db.String(50), unique=True, nullable=False)
-    location = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(50), nullable=False)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime,  
@@ -36,11 +37,11 @@ class Business(db.Model):
 class Reviews(db.Model):
     __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
-    title= db.Column(db.String(50), unique=True, nullable=False)
-    review= db.Column(db.String(50), unique=True, nullable=False)
+    title= db.Column(db.String(50),  nullable=False)
+    review= db.Column(db.String(50),  nullable=False)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    business_id = db.column(db.Integer, db.ForeignKey('business.id'))
+    business_id = db.Column(db.Integer, db.ForeignKey('business.id'))
     date_modified = db.Column(db.DateTime,
         default=db.func.current_timestamp(),
         onupdate= db.func.current_timestamp())
