c96eb19bd00004e1083ca293405cd1e6c8547a57
james947 <jmuriuki947@gmail.com>
2018-04-10 21:23:02 +0300

    Refactor review tests

--- tests/test_reviews.py
+++ tests/test_reviews.py
@@ -1,38 +1,40 @@
-# import json
-# import unittest
+import json
+import unittest
+from source.routes.api import create_app, db
+from source.routes.auth import views
 
-# from source.models.users import User
-# from source.routes.api import app
 
-# class TestIntegrations(unittest.TestCase):
-#     """app test config"""
-#     def setUp(self):
-#         app.testing=True
-#         self.app = app.test_client()
-#         self.reviews = {
-#             'title': "App on point",
-#             'descritpion':  'Your app is awesome',
-#             'business_id':'1'
-#         }
-    
+class TestUsersTestcase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.reviews = {'title': 'App on point','review':  'Your app is awesome'}
+        self.person = {'username':'james', 'email':'jima@gmail.com', 'password':'12345'}
+        self.business = {'businessname':'techbase', 'description':'sell cars', 'category':'cars', 'location':'mombasa road'}
 
-#     def test_add_new_review(self):
-#         """
-#         Api creates a new review
-#         """
-#         response=self.app.post('/api/v1/business/<int:business_id>/review', data=self.reviews)
-#         self.assertEqual(response.status_code,201)
-#         response_msg = json.loads(response.data.decode())
-#         self.assertIn("Review Added Successfully", response_msg["Message"]) 
+    def test_add_new_review(self):
+        """
+        Api creates a new review
+        """
+        response = self.client().post('/api/auth/v1/register', data=json.dumps(self.person), headers={'content-type':"application/json"})
+        login = self.client().post('/api/v1/login', data=json.dumps(dict(username="james",password="123456")), content_type="application/json")
+        data = json.loads(login.data.decode())
+        token = data['token']
+        response = self.client().post('/api/v1/business', data=json.dumps(self.business), content_type="application/json", headers={"x-access-token":token})
+        resp=self.client().post('/api/v1/business/1/review', data=json.dumps(self.reviews), content_type="application/json" ,headers={"x-access-token":token})
+        response_msg = json.loads(resp.data.decode())
+        self.assertIn("Review Added Successfully", response_msg["message"]) 
 
-#     def test_add_null_review_title(self):
-#         """"
-#         tests APi result if preview title is null
-#         """
-#         response = self.app.post('/api/auth/v1/business/1/reviews',data=dict(title="",description="Your app is great"))
-#         self.assertEqual(response.status_code,401)
-#         response_msg = json.loads(response.data.decode())
-#         self.assertIn("Title is required", response_msg["Message"]) 
+    def test_add_null_review_title(self):
+        """testsif review title is empty"""
+        response = self.client().post('/api/auth/v1/register', data=json.dumps(self.person), headers={'content-type':"application/json"})
+        login = self.client().post('/api/v1/login', data=json.dumps(dict(username="james",password="123456")), content_type="application/json")
+        data = json.loads(login.data.decode())
+        token = data['token']
+        resp = self.client().post('/api/v1/business', data=json.dumps(self.business), content_type="application/json", headers={"x-access-token":token})
+        response = self.client().post('/api/v1/business/1/review', data=json.dumps(dict(title="", review="Your app is great")), content_type="application/json", headers={"x-access-token":token})
+        response_msg = json.loads(response.data.decode())
+        self.assertIn("Title is required", response_msg["message"]) 
 
 #     def test_add_null_review_description(self):
 #         """"
@@ -102,5 +104,3 @@
 #         del self.reviews
 
 
-# if __name__ == '__main__':
-#      app.run(debug=True)
