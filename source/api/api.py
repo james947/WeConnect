9e2b4bc0a199d48c1a21f6e4a9931bcf2fd600c3
james947 <jmuriuki947@gmail.com>
2018-04-24 19:12:54 +0300

    Add user and password validations

--- source/api/api.py
+++ source/api/api.py
@@ -6,22 +6,30 @@ from passlib.hash import sha256_crypt
 from source.api import validate
 import datetime
 import re
-from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity
-)
-
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, get_raw_jwt
 
 
 app = Flask(__name__)
 """secret key for encoding ofthe token"""
 app.config['SECRET_KEY'] ="b'd45871881ac4561fb7bf9226e27137708e28c505fc21efb9'"
 
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
+
 jwt = JWTManager(app)
+
 
 BUSINESS = []
 USERS = []
 REVIEWS = []
+
+blacklist = set()
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return jti in blacklist
+
     
 @app.route('/api/v1/auth/register', methods=['POST'])
 def create_user():
@@ -34,26 +42,30 @@ def create_user():
     password = user.get('password')
     available_emails = [x.email for x in USERS]
     dict_data = {'Email': email, 'Username':username, 'Password':password}
-   
-    if validate.key_blank(**dict_data):
-        return jsonify(validate.key_blank(**dict_data)), 401
+    
+    msg = validate.key_blank(**dict_data)
+    if msg:
+        return jsonify(msg), 401
+    if validate.key_username(username=dict_data['Username']):
+        return jsonify(validate.key_username(**dict_data))
+    if validate.key_password(password=dict_data['Password']):
+        return jsonify(validate.key_username(**dict_data))
     if validate.blank(**dict_data):
         return jsonify(validate.blank(**dict_data)), 401
     if validate.email(email):   
         return jsonify(validate.email(email)), 401    
     if email in available_emails:
-        return jsonify({'message':'Email is already registered'}), 400
+        return jsonify({'message': 'Email is already registered'}), 400
 
-    password=sha256_crypt.encrypt(str(password))
-    new_user=User(username, email, password)
+    password = sha256_crypt.encrypt(str(password))
+    new_user = User(username, email, password)
     USERS.append(new_user)
-    return jsonify({'message':'User successfully registered'}), 201
+    return jsonify({'message': 'User successfully registered'}), 201
 
-@app.route('/api/v1/auth/login', methods = ['POST'])
+@app.route('/api/v1/auth/login', methods=['POST'])
 def login():
     """if request is validated then user is logged in."""
     user_request = request.get_json()
-    session.pop('current_user', None)
     email = user_request.get('email')
     password = user_request.get('password')
     dict_data = {'Email': email, 'Password':password}
@@ -71,35 +83,37 @@ def login():
         is_user_password = sha256_crypt.verify(str(password), user_login.password)
         if is_user_password and email == user_login.email:
             access_token = create_access_token(identity=email)
-            return jsonify(access_token), 200
-        return jsonify({'message':'Password not correct'})
-    return jsonify({'message':'Email not found'}), 401
+            response = {'token': access_token}
+            return jsonify(response), 200
+        return jsonify({'message': 'Password not correct'})
+    return jsonify({'message': 'Email not found'}), 401
 
 
-@app.route('/api/v1/auth/logout' ,methods=["DELETE"])
+@app.route('/api/v1/auth/logout', methods=["DELETE"])
+@jwt_required
 def logout():
-    """clears sessions"""
-    session.pop('current_user', None)
-    return jsonify({'message':'Logged out successfuly'})
+    dumps = get_raw_jwt()['jti']
+    blacklist.add(dumps)
+    return jsonify({"msg": "Successfully logged out"}), 200
 
-@app.route('/api/v1/auth/reset-password', methods = ['PUT'])
+@app.route('/api/v1/auth/reset-password', methods=['PUT'])
 @jwt_required
 def reset_password():
     """Resets password"""
     reset = request.get_json()
     current_password = reset.get('current_paswword')
     new_password = reset.get('new_password')
-    dict_data = {'current_password': current_password, 'new_password':new_password}
+    dict_data = {'current_password': current_password, 'new_password': new_password}
 
     if validate.key_blank(**dict_data):
         return jsonify(validate.key_blank(**dict_data)), 401
     if validate.blank(**dict_data):
         return jsonify(validate.blank(**dict_data)), 401
-    #confirm password
+    # confirm password
     get_user = [user for user in USERS if user.password == current_password]
     found_user = get_user[0]
     found_user.password = new_password
-    return jsonify({'message':'Password reset success'})
+    return jsonify({'message': 'Password reset success'})
 
 
 @app.route('/api/v1/users', methods=['GET'])
@@ -125,8 +139,8 @@ def register_business():
     description = new_business.get('description')
     category = new_business.get('category')
     location = new_business.get('location')
-    dict_data = {'Businessname': businessname, 'Description':description, 
-    'Category':category, 'Location':location}
+    dict_data = {'Businessname': businessname, 'Description': description, 
+    'Category': category, 'Location': location}
 
     if validate.key_blank(**dict_data):
         return jsonify(validate.key_blank(**dict_data)), 401
@@ -145,94 +159,97 @@ def register_business():
 @app.route('/api/v1/business/', methods=['GET'])
 def get_all_businesses():
     """Returns the requested business all the registered businesses"""
-    business = BUSINESS
-    found_business =[{'businessname': business.businessname, 'description':business.description,
-    'category': business.category,'location':business.location ,'id':business.id } for business in BUSINESS]
-    return jsonify(found_business),200                
+    get_business = [business for business in BUSINESS]
+    if not get_business:
+            return jsonify({'message':'Business not found'})
+    found_business = [{'businessname': business.businessname, 'description': business.description,
+    'category': business.category, 'location': business.location, 'id': business.id } for business in BUSINESS]
+    return jsonify(found_business), 200
 
 @app.route('/api/v1/business/<int:business_id>', methods=['GET'])
 def get_by_id(business_id):
     """Gets a particular bsuiness by id"""
-    business =[business for business in BUSINESS if business.id==business_id]
+    business = [business for business in BUSINESS if business.id == business_id]
     if business:
         business = business[0]
     elif business not in BUSINESS: 
-        return jsonify({'message':'Business not found'}), 404
+        return jsonify({'message': 'Business not found'}), 404
     found_business = {
-                    'id':business.id,
-                    'businessname':business.businessname,
-                    'description':business.description,
-                    'category':business.category,
-                    'location':business.location
+                    'id': business.id,
+                    'businessname': business.businessname,
+                    'description': business.description,
+                    'category': business.category,
+                    'location': business.location
                    }
     return jsonify(found_business), 200
+
 
 @app.route('/api/v1/business/<int:business_id>', methods=['PUT'])
 @jwt_required
 def update_by_id(business_id):
     """"updates business by id"""
-    new_update = request.get_json()
-    businessname = new_update.get('businessname')
-    description = new_update.get('description')
-    category = new_update.get('category')
-    location = new_update.get('location')
-
-    dict_data = {'Businessname': businessname, 'Description':description, 
-    'Category':category, 'Location':location}
+    dict_data = request.get_json()
 
     if validate.key_blank(**dict_data):
         return jsonify(validate.key_blank(**dict_data)), 401
     if validate.blank(**dict_data):
-        return jsonify(validate.blank(**dict_data)), 401
+        return jsonify(validate.blank(**dict_data)), 4011
 
-    available_business = [biz.businessname for biz in BUSINESS]
-    business = available_business[0]
-    if businessname in business:
+    businessname = dict_data.get('businessname')
+    business_names = [business.businessname for business in BUSINESS]
+    if businessname in business_names:
         return jsonify({'message': 'Business already exists'}), 409
-    business.businessname = businessname
-    print(businessname)
-    business.description = description
-    business.category = category
-    business.location = location
-    updated_business=[{'business.id' :business.id, 'businessname':business.businessname,
-    'description':business.description,'category':business.category,
-    'location':business.location} for business in BUSINESS]
+
+    target_business = None
+    for business in BUSINESS:
+        if business.id == business_id:
+            target_business = business
+            break
+    if not target_business:
+        return jsonify({"message": "Business not found"})   
+    for key in dict_data.keys():
+        value = dict_data[key]
+        setattr(target_business, key, value)
+
+    updated_business = {'business.id': target_business.id, 'businessname': target_business.businessname,
+    'description': target_business.description, 'category': target_business.category,
+    'location': target_business.location} 
     return jsonify(updated_business), 200   
 
 @app.route('/api/v1/business/<int:business_id>', methods=['DELETE'])
 @jwt_required
 def delete_business_by_id(business_id):
     """Endpoint for deleting requested business by id"""
-    target_business= None
+    target_business = None
     for business in BUSINESS:
-        if business.id==business_id:
+        if business.id == business_id:
             target_business = business
-    
+
     if target_business:
         BUSINESS.remove(target_business)
-        return jsonify({'message':'Business successfully deleted'}), 202
-    return jsonify({'message':'Business not found'}), 404
+        return jsonify({'message': 'Business successfully deleted'}), 202
+    return jsonify({'message': 'Business not found'}), 404
 
 @app.route('/api/v1/business/<int:business_id>/review', methods=['POST'])
 def add_review(business_id):
     new_review = request.get_json()
     title = new_review.get('title')
     description = new_review.get('description')
-    dict_data = {'Title': title, 'Description':description}
+    dict_data = {'Title': title, 'Description': description}
 
     if validate.key_blank(**dict_data):
         return jsonify(validate.key_blank(**dict_data)), 401
     if validate.blank(**dict_data):
         return jsonify(validate.blank(**dict_data)), 401
         
-    business=[business for business in BUSINESS if business.id==business_id]
+    business = [business for business in BUSINESS if business.id==business_id]
     if business:
         business = business[0] 
         business_id = business.id
         new_review =Reviews(title,description,business_id)
         REVIEWS.append(new_review)
-        return jsonify({'message':'Review Added Successfully'}), 201
-    return jsonify({'message':'Business not found'}), 404
+        return jsonify({'message': 'Review Added Successfully'}), 201
+    return jsonify({'message': 'Business not found'}), 404
 
 @app.route('/api/v1/business/<int:business_id>/review', methods=['GET'])
 def get_all_reviews(business_id):
@@ -240,7 +257,7 @@ def get_all_reviews(business_id):
     if business:
         business = business[0]
         business_id = business.id
-        review = [{'title':review.title, 'description':review.description} for review in REVIEWS]
+        review = [{'title': review.title, 'description': review.description} for review in REVIEWS]
         return jsonify(review), 200
     return jsonify({'message': 'Reviews not found'}), 404
 
--- source/api/validate.py
+++ source/api/validate.py
@@ -1,17 +1,34 @@
 '''Validations'''
 import re
 
+
 def blank(**data):
     for key in data:
         name = re.sub(r'\s+','',data[key])
         if not name:
+
             return {'message':key + ' is required'}
 def email(data):
     vemail = re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",data)
     if not vemail:
         return {'message':'Email is invalid'}
 
+
 def key_blank(**data):
     for key in data:
         if data[key] is None:
-            return {'message':key + ' is Missing'}
+            return {'message': key + ' is Missing'}
+
+
+def key_username(**data):
+    for key in data:
+        username = re.match(r'^[a-zA-Z_]+[\d\w]{4,}', data[key])
+        if not username:
+            return {'message': key + ' should contain atleast (a-z), (0-4), (-), (_), characters(4)'}
+
+
+def key_password(**data):
+    for key in data:
+        password = re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', data[key])
+        if not password:
+            return {'message': key + ' should contain atleast (a-z), (0-4), (-), (_), characters(8)'}
