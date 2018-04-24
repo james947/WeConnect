f01cf9e490677cfacae2b2ec0d04aa383e322e00
james947 <jmuriuki947@gmail.com>
2018-04-24 19:15:03 +0300

    Refactor tests business

--- tests/test_business.py
+++ tests/test_business.py
@@ -1,31 +1,47 @@
 from base import BaseTestCase
 import json
 
+
 class TestIntegrations(BaseTestCase):
     """tests for business enpoints"""
     def test_business_registration(self):
         """ test business successfully is registered"""
         self.register_user()
-        self.login_user()
-        resp = self.business_registration()
-        response_msg = json.loads(resp.data.decode())
+        login = self.login_user()
+        resp = json.loads(login.data.decode("UTF-8"))
+        token = resp['token']
+        register = self.app.post('/api/v1/business', data=json.dumps(dict(businessname="techbase", description="yoyo", category="blabla", location="runda")),  content_type="application/json", headers={"Authorization":"Bearer {}".format(token)})
+        response_msg = json.loads(register.data.decode())
         self.assertIn("Business successfully registered", response_msg["message"])
 
     def test_returns_all_businesses(self):
         """test all businesses are returned"""
-        self.business_registration()
+        self.register_user()
+        login = self.login_user()
+        resp = json.loads(login.data.decode("UTF-8"))
+        token = resp['token']
+        register =self.app.post('/api/v1/business', 
+        data=json.dumps(dict(businessname="Techbase", description="yoyo", 
+        category="blabla", location="runda")),  content_type="application/json", 
+        headers={"Authorization":"Bearer {}".format(token)})
         resp = self.app.get('/api/v1/business/')
         response_data = json.loads(resp.data.decode('utf-8'))
         registered_business = response_data[0]
         self.assertEqual(registered_business['businessname'], 'Techbase')
         
 
-    # def test_api_can_get_business_by_id(self):
-    #     """get business by id"""
-    #     self.business_registration()
-    #     resp = self.app.get('/api/v1/business/0')
-    #     registered_business = response_data[0]
-    #     self.assertEqual(registered_business['businessname'], 'Techbase')
+    def test_api_can_get_business_by_id(self):
+        self.register_user()
+        login = self.login_user()
+        resp = json.loads(login.data.decode("UTF-8"))
+        token = resp['token']
+        register =self.app.post('/api/v1/business', 
+        data=json.dumps(dict(businessname="Techbase", description="yoyo", 
+        category="blabla", location="runda")),  content_type="application/json", 
+        headers={"Authorization":"Bearer {}".format(token)})
+        get = self.app.get('/api/v1/business/0')
+        response_data = json.loads(get.data.decode('utf-8'))
+        self.assertIn('Techbase', response_data['businessname'])
 
     def test_get_by_ivalid_id(self):
         """tests if the get business by invalid id"""
@@ -36,33 +52,48 @@ class TestIntegrations(BaseTestCase):
 
     def test_add_empty_business_name(self):
         """tests addition of a empty business name"""
+        self.register_user()
+        login = self.login_user()
+        resp = json.loads(login.data.decode("UTF-8"))
+        token = resp['token']
         response = self.app.post('/api/v1/business', 
         data =json.dumps(dict(businessname="", description="yoyo", category="blabla", location="runda")),
-        headers = {'content-type': 'application/json'})
+        headers={"Authorization":"Bearer {}".format(token)},content_type="application/json")
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn("Businessname is required", response_msg['message'])
 
     def test_add_empty_business_category(self):
         """tests addition of a null category"""
+        self.register_user()
+        login = self.login_user()
+        resp = json.loads(login.data.decode("UTF-8"))
+        token = resp['token']
         response = self.app.post('/api/v1/business', 
-        data=json.dumps(dict(businessname="techbase", description="we sell laptops", category="", location="juja")),
-        content_type="application/json")
+        data =json.dumps(dict(businessname="Techbase", description="yoyo", category="", location="runda")),
+        headers={"Authorization":"Bearer {}".format(token)},content_type="application/json")
         response_msg = json.loads(response.data.decode("UTF-8"))
         self.assertIn("Category is required", response_msg['message'])
 
     def test_add_empty_business_location(self):
-        # tests addition of a null location
+        self.register_user()
+        login = self.login_user()
+        resp = json.loads(login.data.decode("UTF-8"))
+        token = resp['token']
         response = self.app.post('/api/v1/business', 
-        data = json.dumps(dict(businessname="techbase", description="we sell laptops", category="electronics", location="")),
-         content_type="application/json")
+        data =json.dumps(dict(businessname="Techbase", description="yoyo", category="laptops", location="")),
+        headers={"Authorization":"Bearer {}".format(token)},content_type="application/json")
         response_msg = json.loads(response.data.decode())
         self.assertIn("Location is required", response_msg['message'])
 
     def test_add_empty_business_description(self):
         """tests addition of a null description"""
+        self.register_user()
+        login = self.login_user()
+        resp = json.loads(login.data.decode("UTF-8"))
+        token = resp['token']
         response = self.app.post('/api/v1/business', 
-        data=json.dumps(dict(businessname="techbase", description="", category="electronics", location="juja")),
-        content_type="application/json")
+        data =json.dumps(dict(businessname="Techbase", description="", category="laptops", location="runda")),
+        headers={"Authorization":"Bearer {}".format(token)},content_type="application/json")
         response_msg = json.loads(response.data.decode())
         self.assertIn("Description is required",response_msg['message'])
 
@@ -76,15 +107,20 @@ class TestIntegrations(BaseTestCase):
         data=json.dumps(dict(businessname="Ramtoms", description="sell iron boxes", category="electronics", location="juja")), 
         content_type="application/json")
         results = self.app.get('/api/v1/business/0')
+        print('hhijjmo',results)
         self.assertIn(results.content_type, 'application/json')
 
     def test_delete(self):
         """test API can delete business"""
         self.register_user()
-        self.login_user()
-        resp=self.business_registration()
-        print(resp)
-        response=self.app.delete('/api/v1/business/0')
+        login = self.login_user()
+        resp= json.loads(login.data.decode('UTF-8'))
+        token = resp['token']
+        new_buisness = self.app.post('/api/v1/business', 
+        data =json.dumps(dict(businessname="Techbase", description="lala", category="laptops", location="runda")),
+        headers={"Authorization":"Bearer {}".format(token)}, content_type="application/json")
+        response=self.app.delete('/api/v1/business/0', 
+        headers={"Authorization":"Bearer {}".format(token)}, content_type="application/json")
         response_msg = json.loads(response.data.decode())
         self.assertIn("Business successfully deleted", response_msg["message"])
 
