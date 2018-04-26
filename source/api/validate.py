99d224c998dbff2929a404efd59b9966cbc561da
james947 <jmuriuki947@gmail.com>
2018-04-26 14:52:47 +0300

    Add username and password regex

--- source/api/validate.py
+++ source/api/validate.py
@@ -4,31 +4,30 @@ import re
 
 def blank(**data):
     for key in data:
-        name = re.sub(r'\s+','',data[key])
+        name = re.sub(r'\s+', '', data[key])
         if not name:
+            return {'message': key + ' is required'}
 
-            return {'message':key + ' is required'}
+
 def email(data):
     vemail = re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",data)
     if not vemail:
-        return {'message':'Email is invalid'}
+        return {'message': 'Email is invalid'}
 
 
 def key_blank(**data):
     for key in data:
         if data[key] is None:
-            return {'message': key + ' is Missing'}
+            return {'message': key + ' is missing'}
 
 
-def key_username(**data):
-    for key in data:
-        username = re.match(r'^[a-zA-Z_]+[\d\w]{4,}', data[key])
-        if not username:
-            return {'message': key + ' should contain atleast (a-z), (0-4), (-), (_), characters(4)'}
+def key_username(username):
+    match = re.match(r'^[a-zA-Z_]+[\d\w]{3,}', username)
+    if match is None:
+        return {'message': 'Username should contain atleast (a-z), (0-4), (-), (_), characters(4)'} 
 
 
-def key_password(**data):
-    for key in data:
-        password = re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', data[key])
-        if not password:
-            return {'message': key + ' should contain atleast (a-z), (0-4), (-), (_), characters(8)'}
+def key_password(password):
+    password = re.match(r'[a-zA-Z_]+[\d\w]{8,}', password)
+    if password is None:
+        return {'message': 'password should contain atleast (a-z), (0-4), (-), characters(8)'}
