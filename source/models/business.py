fd501e6c6adf048bb5942cfe26b9868c9bec6a0b
james947 <jmuriuki947@gmail.com>
2018-04-26 14:56:03 +0300

    Refactor business model class

--- source/models/business.py
+++ source/models/business.py
@@ -1,10 +1,10 @@
 class Business():
     count = 0
-     #initialize count to add when business is registered
+    #initialize count to add when business is registered
     def __init__(self,businessname,description,location,category):
         self.businessname = businessname
         self.description = description
         self.location = location
         self.category = category
         self.id = Business.count
-        Business.count +=1
+        Business.count += 1
