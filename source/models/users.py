99f760a026937b619c83e0556748467960a3fd82
james947 <jmuriuki947@gmail.com>
2018-04-26 14:57:01 +0300

    Add users tests

--- source/models/users.py
+++ source/models/users.py
@@ -1,11 +1,9 @@
 class User:
-    count =0 #icrease user id when user signs up
-    
+    count =0 #icrease user id when user signs up   
     def __init__(self,username,email,password):
         self.username = username
         self.email =email
         self.password = password
         self.id = User.count
-        User.count +=1
-    
+        User.count += 1
     
