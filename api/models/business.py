a48f187e6a8eb289b0bb67c3939d2ad9da8020d8
james947 <jmuriuki947@gmail.com>
2018-05-12 11:45:39 +0300

    Remove whitespaces

--- api/models/business.py
+++ api/models/business.py
@@ -1,7 +1,7 @@
-class Business():
-    count = 0
-    #initialize count to add when business is registered
-    def __init__(self,businessname,description,location,category):
+class Business():  
+    count = 0  # initialize count to add when business is registered
+
+    def __init__(self, businessname, description, location, category):
         self.businessname = businessname
         self.description = description
         self.location = location
--- api/models/reviews.py
+++ api/models/reviews.py
@@ -1,9 +1,10 @@
 class Reviews():
     """increments review by one"""
     count = 0
-    def __init__(self,title,description,business_id):
-        self.title=title
+
+    def __init__(self, title, description, business_id):
+        self.title = title
         self.description = description
         self.business_id = business_id
         self.id = Reviews.count
-        Reviews.count+=1
+        Reviews.count += 1
