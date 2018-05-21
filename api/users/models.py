64305fc9c8e62c6a785e1975ece9974f57fbe435
james947 <jmuriuki947@gmail.com>
2018-05-21 20:38:54 +0300

    [ADD] new user model for token blacklisting

--- api/users/models.py
+++ api/users/models.py
@@ -18,3 +18,22 @@ class Users(db.Model):
     businesses = db.relationship('Business',backref='owner', lazy='dynamic')
 
     reviews = db.relationship('Reviews', backref='reviewer', lazy='dynamic')
+
+class Blacklist(db.Model):
+
+    __tablename__ = "blacklist"
+
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String, nullable=False)
+
+
+    def __init__(self,token):
+        self.token = token
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
