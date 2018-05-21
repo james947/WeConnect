03b865bb29b1d11ce6cef7fefd703b3eba09cb6a
james947 <jmuriuki947@gmail.com>
2018-05-21 20:38:10 +0300

    [ADD]function to validate token

--- api/helpers/token.py
+++ api/helpers/token.py
@@ -1,8 +1,25 @@
 from functools import wraps
 import jwt
 import os
-from api.users.models import Users
+from api.users.models import Users, Blacklist
 from flask import Flask, jsonify, abort, request, make_response, json, session, Blueprint
+
+
+def validate_token(token):
+    isError = False
+
+    check_token = Blacklist.query.filter_by(token=token).first()
+    if check_token:
+        isError = True
+        return "Token Blacklisted", isError
+
+    try:
+        payload = jwt.decode(token, os.getenv('SECRET'))
+        return "Valid token", isError
+    except jwt.ExpiredSignatureError:
+        isError = True
+        return 'Token expired. Please log in again', isError
+
 
 def token_required(f):
     """Decorator to secure all endpoints"""
@@ -12,8 +29,13 @@ def token_required(f):
   
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
+
         if not token:
             return jsonify({'message': 'Token is required'}), 401
+
+        message, isError = validate_token(token)
+        if isError:
+            return jsonify({"message": message}), 401
 
         try:
             data = jwt.decode(token, os.getenv('SECRET'))
@@ -21,6 +43,7 @@ def token_required(f):
         except:
             return jsonify({'message': 'Token is invalid!'}), 401
 
-        return f(current_user, *args, **kwargs)
+        return f(current_user=current_user, *args, **kwargs)
 
     return decorated
+
